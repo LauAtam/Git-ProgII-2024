c191794dc675b07a59df48ac377b48b13651b624
LauAtam <lautaroatampiz@gmail.com>
2024-09-11 00:47:02 -0300

    Correccion en el pasaje de parametros de los detalles

--- U1 - Problema 5/Datos/Repositorios/FacturaRepository.cs
+++ U1 - Problema 5/Datos/Repositorios/FacturaRepository.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using U1___Problema_5_v2.Dominio;
 
 namespace U1___Problema_5.Datos.Repositorios
 {
-    public class FacturaRepository
+    public class FacturaRepository : Repository<Factura>
     {
-        private DBHelper _helper;
-        public FacturaRepository()
+        public FacturaRepository() : base()
         {
-            _helper = DBHelper.ObtenerInstancia();
+
+        }
+        public override bool Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Guardar(Factura factura)
+        {
+            int contadorDetalles = 0;
+            string spMaestro = "SP_GUARDAR_FACTURA";
+            string spDetalle = "SP_GUARDAR_DETALLE_FACTURA";
+            List<SqlParameter> maestro = new List<SqlParameter>()
+            {
+                new SqlParameter("@id", factura.Id),
+                new SqlParameter("@id_forma_pago", factura.FormaPago.Id),
+                new SqlParameter("@nombre_cliente", factura.NombreCliente),
+            };
+            List<List<SqlParameter>> detalles = new List<List<SqlParameter>>();
+            foreach (DetalleFactura detalle in factura.Detalles)
+            {
+                contadorDetalles++;
+                detalles.Add(new List<SqlParameter>()
+                {
+                    new SqlParameter("@id", detalle.Id),
+                    new SqlParameter("@nro_detalle", contadorDetalles),
+                    new SqlParameter("@id_articulo", detalle.Articulo.Id),
+                    new SqlParameter("@cantidad", detalle.Cantidad)
+                });
+            }
+            return _helper.EjecutarSPDMLMaestroDetalles(spMaestro, spDetalle, maestro, detalles);
+        }
+
+        public override Factura ObtenerPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Factura> ObtenerTodos()
+        {
+            throw new NotImplementedException();
         }
     }
 }
